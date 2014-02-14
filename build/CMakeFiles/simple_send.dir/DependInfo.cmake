3730d7e052713f7417ace84f19d5618fbdb63fd5
Haikal Pribadi <haikal.pribadi@gmail.com>
2014-02-14 17:56:55 +0000

    first commit

---
+++ build/CMakeFiles/simple_send.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/haikalpribadi/Workspace/ROS/radiophonic/oscpack/src/SimpleSend.cpp" "/home/haikalpribadi/Workspace/ROS/radiophonic/oscpack/build/CMakeFiles/simple_send.dir/src/SimpleSend.cpp.o"
+  "/home/haikalpribadi/Workspace/ROS/radiophonic/oscpack/src/ip/IpEndpointName.cpp" "/home/haikalpribadi/Workspace/ROS/radiophonic/oscpack/build/CMakeFiles/simple_send.dir/src/ip/IpEndpointName.cpp.o"
+  "/home/haikalpribadi/Workspace/ROS/radiophonic/oscpack/src/ip/NetworkingUtils.cpp" "/home/haikalpribadi/Workspace/ROS/radiophonic/oscpack/build/CMakeFiles/simple_send.dir/src/ip/NetworkingUtils.cpp.o"
+  "/home/haikalpribadi/Workspace/ROS/radiophonic/oscpack/src/ip/UdpSocket.cpp" "/home/haikalpribadi/Workspace/ROS/radiophonic/oscpack/build/CMakeFiles/simple_send.dir/src/ip/UdpSocket.cpp.o"
+  "/home/haikalpribadi/Workspace/ROS/radiophonic/oscpack/src/osc/OscOutboundPacketStream.cpp" "/home/haikalpribadi/Workspace/ROS/radiophonic/oscpack/build/CMakeFiles/simple_send.dir/src/osc/OscOutboundPacketStream.cpp.o"
+  "/home/haikalpribadi/Workspace/ROS/radiophonic/oscpack/src/osc/OscPrintReceivedElements.cpp" "/home/haikalpribadi/Workspace/ROS/radiophonic/oscpack/build/CMakeFiles/simple_send.dir/src/osc/OscPrintReceivedElements.cpp.o"
+  "/home/haikalpribadi/Workspace/ROS/radiophonic/oscpack/src/osc/OscReceivedElements.cpp" "/home/haikalpribadi/Workspace/ROS/radiophonic/oscpack/build/CMakeFiles/simple_send.dir/src/osc/OscReceivedElements.cpp.o"
+  "/home/haikalpribadi/Workspace/ROS/radiophonic/oscpack/src/osc/OscTypes.cpp" "/home/haikalpribadi/Workspace/ROS/radiophonic/oscpack/build/CMakeFiles/simple_send.dir/src/osc/OscTypes.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
---
+++ build/CMakeFiles/simple_send.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/simple_send.dir/src/SimpleSend.cpp.o"
+  "CMakeFiles/simple_send.dir/src/ip/IpEndpointName.cpp.o"
+  "CMakeFiles/simple_send.dir/src/ip/UdpSocket.cpp.o"
+  "CMakeFiles/simple_send.dir/src/ip/NetworkingUtils.cpp.o"
+  "CMakeFiles/simple_send.dir/src/osc/OscOutboundPacketStream.cpp.o"
+  "CMakeFiles/simple_send.dir/src/osc/OscPrintReceivedElements.cpp.o"
+  "CMakeFiles/simple_send.dir/src/osc/OscReceivedElements.cpp.o"
+  "CMakeFiles/simple_send.dir/src/osc/OscTypes.cpp.o"
+  "../bin/simple_send.pdb"
+  "../bin/simple_send"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/simple_send.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
